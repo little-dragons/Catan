5c08468c03a5870aeb438883408bf48ebda8dd7f
IchiganCS <mail@ichigancs.com>
2024-05-20 13:36:46 +0200

    Test for production configuration

--- server/Server.ts
+++ server/Server.ts
@@ -1,13 +1,30 @@
 import { BuildingType, ClientEventMap, Color, ConnectionError, GuestLogin, MemberLogin, ServerEventMap, SocketPort, UserWithAuth, defaultBoard } from "shared"
 import { Server } from "socket.io"
 import { addGuest, checkUser, removeUser } from "./authentication/AuthIdMap"
+import { createServer, Server as HttpsServer } from 'https'
+import { readFileSync } from  'fs'
 
-const io = new Server<ServerEventMap, ClientEventMap, {}, UserWithAuth | 'anonymous'>(SocketPort, {
+
+let httpsServer: HttpsServer<any, any> = undefined!
+if (process.env.NODE_ENV == 'development')
+    httpsServer = createServer()
+else if (process.env.NODE_ENV == 'production')
+    httpsServer = createServer({
+        key: readFileSync(`${process.env.SSL_DIR}/priv.key`),
+        cert: readFileSync(`${process.env.SSL_DIR}/ssl/cert.cer`)
+    })
+else
+    console.error('NO ENVIRONMENT WAS GIVEN, CANNOT PROCEED')
+
+const io = new Server<ServerEventMap, ClientEventMap, {}, UserWithAuth | 'anonymous'>(httpsServer, {
     cors: {
         origin: '*',
         allowedHeaders: ['Access-Control-Allow-Origin']
-    }
+    },
+    
 })
+
+io.listen(SocketPort)
 
 console.log('Server is listening.')
 
