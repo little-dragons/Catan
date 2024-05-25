8d5e249215351c6945789346c58ae30d26df5846
IchiganCS <mail@ichigancs.com>
2024-05-25 09:12:49 +0200

    Added a button in dev mode for faster room creation

--- client/src/home/Home.vue
+++ client/src/home/Home.vue
@@ -1,7 +1,21 @@
 <script setup lang="ts">
-import { currentAuthUser, currentUser } from '@/socketWrapper/Login';
+import { currentAuthUser, currentUser, sendLogin } from '@/socketWrapper/Login';
 import { sendLogout } from '@/socketWrapper/Logout';
+import { createRoomAndRedirect, currentRoom } from '@/socketWrapper/Room';
+import { isDevelopment } from '@/misc/Globals';
+import { watch } from 'vue';
+import { newRandomRoomId } from 'shared';
 
+function debugLogin() {
+    sendLogin({
+        type: 'guest',
+        name: `debugUser${newRandomRoomId()}`
+    })
+
+    watch(currentAuthUser, () => {
+        createRoomAndRedirect('debugRoom${newRandomRoomId()}')
+    }, { once: true })
+}
 
 </script>
 
@@ -10,4 +24,7 @@ import { sendLogout } from '@/socketWrapper/Logout';
     <h1>Home</h1>
 
     <button @click="sendLogout" v-if="currentUser.status == 'logged in'">Logout</button>
+    <button v-if="isDevelopment" @click="debugLogin" :disabled="currentAuthUser != undefined || currentRoom != undefined">
+        Login and create debug room
+    </button>
 </template>
