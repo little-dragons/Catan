0c5bc2e8302167add4cb1f25ab991a598f643424
Evelyn <borthevelyn@gmail.com>
2024-12-22 17:22:06 +0100

    Development cards

--- client/src/misc/CardTextures.ts
+++ client/src/misc/CardTextures.ts
@@ -3,6 +3,12 @@ import grainCard from '@/assets/resource-cards/grain-card.svg'
 import lumberCard from '@/assets/resource-cards/lumber-card.svg'
 import oreCard from '@/assets/resource-cards/ore-card.svg'
 import woolCard from '@/assets/resource-cards/wool-card.svg'
+import knightCard from '@/assets/dev-cards/knight-card.svg'
+import monopolyCard from '@/assets/dev-cards/monopoly-card.svg'
+import roadBuildingCard from '@/assets/dev-cards/road-building-card.svg'
+import victoryPointCard from '@/assets/dev-cards/victory-point-card.svg'
+import yearOfPlentyCard from '@/assets/dev-cards/year-of-plenty-card.svg'
+
 import { DevCardType, Resource } from 'shared'
 
 export function imageForResource(res: Resource): string {
@@ -16,6 +22,11 @@ export function imageForResource(res: Resource): string {
 }
 
 export function imageForDevCard(card: DevCardType): string {
-    return ''
-    // TODO
+    switch (card) {
+        case DevCardType.Knight: return knightCard;
+        case DevCardType.Monopoly: return monopolyCard;
+        case DevCardType.RoadBuilding: return roadBuildingCard;
+        case DevCardType.VictoryPoint: return victoryPointCard;
+        case DevCardType.YearOfPlenty: return yearOfPlentyCard;
+    }
 }
