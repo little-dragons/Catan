2ab90146e409accdbbaa2d427fe72345dab86d52
IchiganCS <mail@ichigancs.com>
2026-05-22 22:09:23 +0200

    Removed unused `Unpure`

--- shared/Pure.ts
+++ shared/Pure.ts
@@ -24,26 +24,3 @@ export type Pure<T> = T extends Primitive
   : T extends object
   ? PureObject<T>
   : T;
-
-type UnpureObject<T> = { -readonly [K in keyof T]: Unpure<T[K]> }
-
-export type Unpure<T> = T extends Primitive
-  ? T
-  : T extends [infer H] 
-  ? readonly [Unpure<H>]
-  : T extends [infer H, ...infer T] 
-  ?  readonly [Unpure<H>, ...Unpure<T>] 
-  : T extends Map<infer K, infer V>
-  ? Map<Unpure<K>, Unpure<V>>
-  : T extends ReadonlyMap<infer K, infer V>
-  ? Map<Unpure<K>, Unpure<V>>
-  : T extends Set<infer M>
-  ? Set<Unpure<M>>
-  : T extends ReadonlySet<infer M>
-  ? Set<Unpure<M>>
-  : T extends Function
-  ? T
-  : T extends object
-  ? UnpureObject<T>
-  : T;
-
